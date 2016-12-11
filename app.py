76980568402b476063f589890877fec9fb445778
Vishnu <vishnu1991@users.noreply.github.com>
2016-12-11 14:37:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,48 +41,7 @@ def webhook():
 
                     send_message(sender_id, "got it, thanks!")
 
-                    function kittenMessage(recipientId, text) {
-    
-    text = text || "";
-    var values = text.split(' ');
-    
-    if (values.length === 3 && values[0] === 'kitten') {
-        if (Number(values[1]) > 0 && Number(values[2]) > 0) {
-            
-            var imageUrl = "https://placekitten.com/" + Number(values[1]) + "/" + Number(values[2]);
-            
-            message = {
-                "attachment": {
-                    "type": "template",
-                    "payload": {
-                        "template_type": "generic",
-                        "elements": [{
-                            "title": "Kitten",
-                            "subtitle": "Cute kitten picture",
-                            "image_url": imageUrl ,
-                            "buttons": [{
-                                "type": "web_url",
-                                "url": imageUrl,
-                                "title": "Show kitten"
-                                }, {
-                                "type": "postback",
-                                "title": "I like this",
-                                "payload": "User " + recipientId + " likes kitten " + imageUrl,
-                            }]
-                        }]
-                    }
-                }
-            };
-    
-            sendMessage(recipientId, message);
-            
-            return true;
-        }
-    }
-    
-    return false;
-    
-};
+                
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
 
@@ -93,6 +52,8 @@ def webhook():
                     pass
 
     return "ok", 200
+
+
 
 
 def send_message(recipient_id, message_text):
